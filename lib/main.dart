7d30f58524a0e163de6bb8b4db955bad09b1539f
abd.hamid <hamnsxx@gmail.com>
2026-04-28 22:00:57 +0700

    mengubah di bagian home agar langsung membuka di halaman login

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:out_tracker/screens/login_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,14 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-
-
-      home: const Scaffold(
-        body: Center(
-          child: Text('Fondasi Aplikasi Siap!'),
-
-        ),
-      ),
-    );
+        home: const LoginScreen(),
+      );
   }
 }
