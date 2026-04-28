7cb8b93cf321dcf4a1acc3f06789fa7e1676dc8e
abd.hamid <hamnsxx@gmail.com>
2026-04-28 21:38:23 +0700

    menambahkkan comment

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -8,20 +8,24 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  // Controller untuk mengambil input dari TextField
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
+// fungsinya utk mengcek loginn
   void _cekLogin() {
     String username = _usernameController.text;
     String password = _passwordController.text;
-
+// cek apakah username dan password benar
     if (username == 'admin' && password == '12345') {
+      // Jika login berhasil, tampilkan pesan sukses di bawah layarr (Snackbar)
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(content: Text('Login Berhasil!')),
       );
 
 
     } else {
+      //jika salah,munculkaann pesann erorr
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(content: Text('Username atau Password salah!'))
       );
@@ -36,24 +40,24 @@ class _LoginScreenState extends State<LoginScreen> {
         backgroundColor: Colors.blue,
       ),
       body: Padding(
-        padding: const EdgeInsets.all(20.0),
+        padding: const EdgeInsets.all(20.0),// memberikan jarak dari tepi layarr
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,// pposisi widget di tengah layar
           children: [
             TextField(
               controller: _usernameController,
               decoration: const InputDecoration(
                 labelText: 'Username',
-                border: OutlineInputBorder(),
+                border: OutlineInputBorder(), // menmberikan border pada TextField
               ),
             ),
-            const SizedBox(height: 20),
+            const SizedBox(height: 15),// memberikan jarak antar widget
 
             SizedBox(
-              width: double.infinity,
+              width: double.infinity, // membuat tombol memenuhi lebar layar
               height: 50,
               child: ElevatedButton(
-                onPressed: _cekLogin,
+                onPressed: _cekLogin, // memanggil fungsi cekLogin saat tombol ditekan
                 child: const Text('MASUK', style: TextStyle(fontSize: 18)),
               ),
             ),
