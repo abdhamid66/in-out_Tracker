faeef8beb9efa60fa07251262b6c6838c49ef4ce
abd.hamid <hamnsxx@gmail.com>
2026-04-29 12:04:28 +0700

    menambahkan comment

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../models/transaksi.dart';
+import '../models/transaksi.dart'; // untuk mengimporr model transakssii yang sudahh dibuat
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -9,8 +9,10 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  // daftar transaksi yang akan di tammpilkann di halamann home, ini masih kosong karena belum ada input transaksi baru
   List<Transaksi> daftarTransaksi = [];
 
+  // fungsii otomatis untuk menghitung total uang masuk
   double get _totalPemasukan {
     double total = 0;
     for (var item in daftarTransaksi) {
@@ -21,6 +23,7 @@ class _HomeScreenState extends State<HomeScreen> {
     return total;
   }
 
+  // fungsii otomatis untuk menghitung total uang keluar
   double get _totalPengeluaran {
     double total = 0;
     for (var item in daftarTransaksi) {
@@ -31,6 +34,7 @@ class _HomeScreenState extends State<HomeScreen> {
     return total;
   }
 
+  // fungsii otomatis untuk menghitungg sisa saldo berdasarkan total pemasukan dan pengeluaran
   double get _saldo => _totalPemasukan - _totalPengeluaran;
 
   @override
@@ -39,12 +43,12 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         title: const Text('Dashboard In-Out Tracker'),
         backgroundColor: Colors.blue,
-
+        // tomboll logout di pojok kanan atass
         actions: [
           IconButton(
             icon: const Icon(Icons.logout),
             onPressed: () {
-
+              // ketika tomboll di tekanm maka akan kemabali ke ahlaman login dna hapus riwayat sebvelumnya
               Navigator.pushReplacementNamed(context, '/');
             },
           )
@@ -54,10 +58,10 @@ class _HomeScreenState extends State<HomeScreen> {
       padding: const EdgeInsets.all(16.0),
       child: Column(
         children: [
-
+          // kotak ringkasann keuangan (card)
           Card(
             color: Colors.blue[50],
-            elevation: 4,
+            elevation: 4, //bayagan
             child: Padding(
               padding: const EdgeInsets.all(20.0),
               child: Column(
@@ -71,12 +75,14 @@ class _HomeScreenState extends State<HomeScreen> {
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
+                        // kolom utk pmasukan
                         Column(
                           children: [
                             const Text('pemasukan', style: TextStyle(color: Colors.green)),
                             Text('Rp $_totalPemasukan', style: const TextStyle(fontWeight: FontWeight.bold)),
                           ]
                         ),
+                        // kolom untuk pengeluaran
                         Column(
                           crossAxisAlignment: CrossAxisAlignment.end,
                           children: [
@@ -92,15 +98,16 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
             const SizedBox(height: 40),
 
+            // tombol tombol menu utama untuk mencatat transaksi baru dan melihat riwayat transaksi
             ElevatedButton.icon(
               onPressed: () {
-
+                // ketiak tombol dii tekan akan mencetak pesan ke konsol, nanti akan dihubungkan ke halaman input transaksi baru
                 print('Ke Halaman Input');
               },
               icon: const Icon(Icons.add_circle_outline),
               label: const Text('Catat Transaksi Baru'),
               style: ElevatedButton.styleFrom(
-                minimumSize: const Size(double.infinity, 50),
+                minimumSize: const Size(double.infinity, 50),// membuat tombol memenuhi lebar layar
               ),
             ),
             const SizedBox(height: 15),
