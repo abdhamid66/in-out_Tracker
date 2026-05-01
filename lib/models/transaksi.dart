189e04e40bd06d66a2b6ecec2a6a8cbdc284d86e
abd.hamid <hamnsxx@gmail.com>
2026-05-01 23:34:05 +0700

    meng update models/transaksi,menambahkan 2 fungsi penerjemah

--- lib/models/transaksi.dart
+++ lib/models/transaksi.dart
@@ -1,3 +1,5 @@
+// File: lib/models/transaksi.dart
+
 class Transaksi {
   final String id;
   final String judul;
@@ -12,4 +14,30 @@ class Transaksi {
     required this.isPemasukan,
     required this.tanggal,
   });
+
+  // PENERJEMAH 1: Dari Aplikasi ke Database (Menyimpan)
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'judul': judul,
+      'nominal': nominal,
+      // Jika isPemasukan itu true, maka simpan 1. Jika false, simpan 0.
+      'isPemasukan': isPemasukan ? 1 : 0, 
+      // Ubah format waktu menjadi Teks (String) agar bisa disimpan
+      'tanggal': tanggal.toIso8601String(), 
+    };
+  }
+
+  // PENERJEMAH 2: Dari Database ke Aplikasi (Membaca)
+  factory Transaksi.fromMap(Map<String, dynamic> map) {
+    return Transaksi(
+      id: map['id'],
+      judul: map['judul'],
+      nominal: map['nominal'],
+      // Jika angka di database adalah 1, maka kembalikan jadi true.
+      isPemasukan: map['isPemasukan'] == 1, 
+      // Ubah kembali Teks menjadi format Waktu (DateTime)
+      tanggal: DateTime.parse(map['tanggal']), 
+    );
+  }
 }
