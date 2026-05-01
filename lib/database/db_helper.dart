f94d1a5da9084b422b48adf4a3de90305dde985f
abd.hamid <hamnsxx@gmail.com>
2026-05-01 21:28:44 +0700

    menambahkan code ke file dtabase

---
+++ lib/database/db_helper.dart
@@ -0,0 +1,39 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class DBHelper {
+  static final DBHelper _instance = DBHelper._internal();
+  factory DBHelper() => _instance;
+  DBHelper._internal();
+
+  static Database? _database;
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDB();
+    return _database!;
+  }
+
+  Future<Database> _initDB() async {
+    String path = join(await getDatabasesPath(), 'in_out_tracker.db');
+
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: _onCreate,
+    );
+  }
+
+  Future<void> _onCreate(Database db, int version) async {
+
+    await db.execute('''
+      CREATE TABLE transaksi (
+        id TEXT PRIMARY KEY,
+        judul TEXT,
+        nominal REAL,
+        isPemasukan INTEGER,
+        tanggal TEXT
+      )
+    ''');
+  }
+}
